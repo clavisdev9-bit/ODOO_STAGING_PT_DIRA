114c2867f0ce6d27daca486330eff6c5f885aa97
usa8bit <usa8bit@gmail.com>
2026-04-27 14:15:48 +0700

    perubahan nama module

---
+++ purchase_order_warning/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Purchase Order Warning',
+    'version': '1.0',
+    'license': 'LGPL-3',
+    'author': 'Usamah',
+    'category': '',
+    'description': """Custom module for WPC Addons""",
+    'depends': ['base','purchase'],
+    'data': [
+        'views/purchase_order_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
